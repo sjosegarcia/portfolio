6ff05f123df7ffbfcff48626addfd585c2fb055a
Steven Garcia <sjosegarcia23@gmail.com>
2022-07-28 21:12:47 -0400

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,45 +1,89 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [color, setColor] = useState<string>("transparent");
+  const [textColor, setTextColor] = useState<string>("white");
+
+  const changeIsOpen = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor("#ffffff");
+        setTextColor("#000000");
+        return;
+      }
+      setColor("transparent");
+      setTextColor("#ffffff");
+    };
+    window.addEventListener("scroll", changeColor);
+  }, []);
+
   return (
-    <div>
-      <div>
+    <div
+      className={`fixed left-0 top-0 z-10 w-full ease-in-out duration-300 bg-${
+        color === "transparent" ? "transparent" : `[${color}]`
+      }`}
+    >
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/">
-          <h1>Captur</h1>
+          <h1 className={`font-bold text-4xl text-[${textColor}]`}>Captur</h1>
         </Link>
-        <ul>
-          <li>
+        <ul className={`hidden sm:flex text-[${textColor}]`}>
+          <li className="p-4">
             <Link href="/">Home</Link>
           </li>
-          <li>
-            <Link href="/">Home</Link>
+          <li className="p-4">
+            <Link href="/#gallery">Gallery</Link>
           </li>
-          <li>
-            <Link href="/">Home</Link>
+          <li className="p-4">
+            <Link href="/work">Work</Link>
           </li>
-          <li>
-            <Link href="/">Home</Link>
+          <li className="p-4">
+            <Link href="/contact">Contact</Link>
           </li>
         </ul>
         {/** Mobile Button */}
-        <div>
-          <AiOutlineMenu size={20} />
+        <div onClick={changeIsOpen} className="block sm:hidden z-10">
+          {isOpen ? (
+            <AiOutlineClose size={20} className={`text-[${textColor}]`} />
+          ) : (
+            <AiOutlineMenu size={20} className={`text-[${textColor}]`} />
+          )}
         </div>
         {/** Mobile Menu */}
-        <div>
+        <div
+          className={`sm:hidden absolute top-0 ${
+            isOpen ? "left-0" : "left-[-100%]"
+          } right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300`}
+        >
           <ul>
-            <li>
+            <li
+              onClick={changeIsOpen}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
               <Link href="/">Home</Link>
             </li>
-            <li>
-              <Link href="/">Home</Link>
+            <li
+              onClick={changeIsOpen}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
+              <Link href="/#gallery">Gallery</Link>
             </li>
-            <li>
-              <Link href="/">Home</Link>
+            <li
+              onClick={changeIsOpen}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
+              <Link href="/work">Work</Link>
             </li>
-            <li>
-              <Link href="/">Home</Link>
+            <li
+              onClick={changeIsOpen}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
+              <Link href="/contact">Contact</Link>
             </li>
           </ul>
         </div>

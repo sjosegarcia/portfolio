daac5937bca7b08d686550ddc6b1326bedb19a74
Steven Garcia <sjosegarcia23@gmail.com>
2022-07-30 09:07:15 -0400

    75% viewport

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,9 +8,9 @@ interface HeroProps {
 const Hero = ({ heading, message }: HeroProps) => {
   return (
     <>
-      <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+      <div className="flex items-center justify-center mb-12 bg-fixed bg-center bg-cover custom-img h-75">
         {/* Overlay */}
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]" />
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2] h-75" />
         <div className="p-5 text-white z-[2] mt-[-10rem]">
           <h2 className="text-5xl font-bold">{heading}</h2>
           <p className="py-5 text-xl">{message}</p>

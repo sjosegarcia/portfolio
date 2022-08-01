0aac0d01fa92d489c72e6e2beed1535cae19dcdc
Steven Garcia <sjosegarcia23@gmail.com>
2022-07-31 21:18:07 -0400

    slide effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Sidebar />
-      <div className="ml-[3.35rem] md:ml-[20rem] flex flex-col">
+      <div className="sidebar ml-[3.35rem] md:ml-[20rem] flex flex-col">
         <Component {...pageProps} />
       </div>
     </>

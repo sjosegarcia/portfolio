436b711213c1114d54d68485c7f26d35d790f3e8
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-02 19:13:09 -0400

    no yarn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/tailwind.css";
 import type { AppProps } from "next/app";
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/Footer";
-import OtherSidebar from "../components/Sidebar";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>

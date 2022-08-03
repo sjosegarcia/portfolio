13a1296ac80c23cb5c5e64faf5264e8df727b7eb
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-03 19:27:35 -0400

    added services folder

---
+++ services/connect.ts
@@ -0,0 +1,7 @@
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+const provider = new WalletConnectProvider({
+  infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+});
+
+export default provider;

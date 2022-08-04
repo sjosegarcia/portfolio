90de1d007edf64592b22dad1884f5b371c592029
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-04 08:35:36 -0400

    env variables

--- services/connect.ts
+++ services/connect.ts
@@ -1,7 +1,9 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const provider = new WalletConnectProvider({
-  infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+  infuraId: process.env.PRODUCTION
+    ? process.env.INFURA_ID
+    : "27e484dcd9e3efcfd25a83a78777cdf1",
 });
 
 export default provider;

6904b5a970cdeab75c5115b011e3a530f0a60629
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-04 20:03:12 -0400

    fixed issue with loading infura id

--- services/connect.ts
+++ services/connect.ts
@@ -1,7 +1,7 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const provider = new WalletConnectProvider({
-  infuraId: process.env.INFURA_ID,
+  infuraId: `${process.env.INFURA_ID}`,
 });
 
 export default provider;

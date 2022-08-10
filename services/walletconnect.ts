15d846b1b919be760bc5468c2a7e0a5d3edf43dc
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-10 10:51:17 -0400

    fixed up the useWalletConnect hook

--- services/walletconnect.ts
+++ services/walletconnect.ts
@@ -2,32 +2,33 @@ import { useState } from "react";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const useWalletConnect = (): [WalletConnectProvider, () => Promise<void>] => {
+const useWalletConnect = (): [
+  WalletConnectProvider | null,
+  () => Promise<WalletConnectProvider>,
+  () => Promise<void>
+] => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [walletConnect, setWalletConnect] = useState<WalletConnectProvider>(
-    new WalletConnectProvider({
-      infuraId: `${process.env.INFURA_ID}`,
-      bridge: "https://bridge.walletconnect.org",
-      qrcode: true,
-    })
-  );
+  const [walletConnect, setWalletConnect] =
+    useState<WalletConnectProvider | null>(null);
 
-  const activate = () => {
+  const createWalletProvider = async (): Promise<WalletConnectProvider> => {
     const walletConnectProvider = new WalletConnectProvider({
-      infuraId: `${process.env.INFURA_ID}`,
+      rpc: { 5: process.env.NEXT_PUBLIC_GOERLI_URL || "" },
+      infuraId: process.env.NEXT_PUBLIC_INFURA_ID || "",
       bridge: "https://bridge.walletconnect.org",
       qrcode: true,
     });
     setWalletConnect(walletConnectProvider);
+    return walletConnectProvider;
   };
 
   const openWalletConnect = async () => {
-    activate();
-    try {
-      await walletConnect.enable();
-    } catch (e) {}
+    console.log(walletConnect);
+    setWalletConnect(await createWalletProvider());
+    // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+    if (walletConnect) await walletConnect.enable().catch((_) => {});
   };
-  return [walletConnect, openWalletConnect];
+  return [walletConnect, createWalletProvider, openWalletConnect];
 };
 
 export default useWalletConnect;

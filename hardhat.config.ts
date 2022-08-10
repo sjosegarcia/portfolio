32e3f7e84228195d0ef866d5d82d702517ae39be
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-10 09:22:49 -0400

    goerli is the actual name

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,8 +25,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    goreli: {
-      url: process.env.GORELI_URL || "",
+    goerli: {
+      url: process.env.GOERLI_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },

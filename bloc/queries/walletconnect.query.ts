9fc9cfdcabfc982674d5c0a5dd040e27a5a94aa9
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-18 11:50:38 -0400

    query

--- bloc/queries/walletconnect.query.ts
+++ bloc/queries/walletconnect.query.ts
@@ -8,6 +8,19 @@ import {
 export class WalletConnectQuery extends Query<WalletConnectState> {
   provider$ = this.select((state) => state.provider);
   web3$ = this.select((state) => state.web3);
+  accounts$ = this.select((state) => {
+    let newAccounts: string[] = [];
+    state.web3?.on(
+      "accountsChanged",
+      (accounts: string[]) => (newAccounts = accounts)
+    );
+    return newAccounts;
+  });
+  chain$ = this.select((state) => {
+    let newChainId: number = -1;
+    state.web3?.on("chainChanged", (chainId: number) => (newChainId = chainId));
+    return newChainId;
+  });
   error$ = this.selectError();
   isLoading$ = this.selectLoading();
   constructor(protected store: WalletConnectStore) {

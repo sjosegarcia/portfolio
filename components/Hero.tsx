fa2f8faf924ac2fde98d783e4f68b8bf0517a43f
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-22 11:41:46 -0400

    next image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,12 +23,14 @@ const Hero: FC = () => {
             </a>
           </div>
         </div>
-        <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
-          <img
-            className="w-80 md:ml-1 ml-24"
+        <div className="">
+          <Image
             alt="iPhone-12"
             src="/images/iPhone-12-Mockup.png"
-          ></img>
+            className="w-80 md:ml-1 ml-24"
+            width={450}
+            height={800}
+          />
         </div>
       </div>
       <Services />

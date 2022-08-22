8d8b0dd06496ac49917cfc3c82b14141f57c832f
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-22 12:44:09 -0400

    space between button and icon

--- components/WalletConnectButton.tsx
+++ components/WalletConnectButton.tsx
@@ -13,7 +13,7 @@ const WalletConnectButton: FC<WalletConnectButtonProps> = ({
 }) => {
   return (
     <div
-      className="inline-flex items-center px-4 py-2 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-gray-900 cursor-pointer"
+      className="inline-flex items-center px-4 py-2 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-gray-900 cursor-pointer space-x-2"
       onClick={onClick}
     >
       <span className="justify-center">{text}</span>

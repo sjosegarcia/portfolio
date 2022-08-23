189d64f310ff78f36b8c4ce5af90070a4cf5b598
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-22 21:22:17 -0400

    newsletter

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -15,7 +15,7 @@ const Newsletter: FC = () => {
           name="email"
           type="email"
           autoComplete="email"
-          className="border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md text-gray-800 font-semibold hover:border-gray-900"
+          className="border border-gray-600 w-1/3 pr-2 pl-2 py-3 mt-2 rounded-md text-gray-800 font-semibold hover:border-gray-900"
         ></input>{" "}
         <div className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900 cursor-pointer">
           <span className="justify-center">Subscribe</span>

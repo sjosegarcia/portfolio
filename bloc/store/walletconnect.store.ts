040a786753aa30a2f7dfe6e9aa6c608809f53282
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-25 11:55:12 -0400

    added resettable flag

--- bloc/store/walletconnect.store.ts
+++ bloc/store/walletconnect.store.ts
@@ -12,7 +12,7 @@ const createInitialState: WalletConnectState = {
   web3: null,
 };
 
-@StoreConfig({ name: "walletconnect" })
+@StoreConfig({ name: "walletconnect", resettable: true })
 export class WalletConnectStore extends Store<WalletConnectState> {
   constructor() {
     super(createInitialState);

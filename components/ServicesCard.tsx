89ccdea4d9494144a2390dda610626f898d552a1
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-25 13:44:33 -0400

    text size increase

--- components/ServicesCard.tsx
+++ components/ServicesCard.tsx
@@ -21,7 +21,7 @@ const ServicesCard: FC<ServicesCardProp> = ({ title, body, icon }) => {
     >
       <div className="py-8 px-12 mb-12 bg-gray-100 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
         <div className="inline-block text-gray-900 mb-4">{icon}</div>
-        <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
+        <h3 className="text-xl leading-normal mb-2 font-semibold text-black">
           {title}
         </h3>
         <p className="text-gray-500">{body}</p>

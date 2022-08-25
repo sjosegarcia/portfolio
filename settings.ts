4137253c8274b44578aaad7a7a842f7696f102c1
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-25 15:08:58 -0400

    added address shortener

--- settings.ts
+++ settings.ts
@@ -1,3 +1,6 @@
 const web3FormPublicAccessKey = "2be134fe-0a11-4020-908a-eba5ec79e0ba";
-
+export const shortenAddress = (address: string): string =>
+  address.slice(0, 5) +
+  "..." +
+  address.slice(address.length - 4, address.length);
 export default web3FormPublicAccessKey;

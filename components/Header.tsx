9f4158972afe45ab515840f15327f8dc32696850
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-29 14:26:54 -0400

    updated header code, one issue need to figure out how to change state onResize

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,55 @@
 import Link from "next/link";
 import React, { FC, useState } from "react";
 import { useWalletConnect } from "../bloc/hooks/walletconnect.hook";
-import WalletAddressDropDown from "./WalletAddressDropDown";
-import WalletConnectButton from "./WalletConnectButton";
+import {
+  Bars4Icon,
+  ChevronDownIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+import { shortenAddress } from "../settings";
 
 const Header: FC = () => {
   const [walletconnect, onOpen, onClose, isLoggedIn] = useWalletConnect();
   const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const loginFlow = () =>
+    !navbarOpen ? (
+      !isLoggedIn ? (
+        <Bars4Icon
+          className="h-20 w-20 text-black cursor-pointer leading-none px-3 py-1 md:hidden outline-none focus:outline-none"
+          onClick={() => setNavbarOpen(!navbarOpen)}
+        />
+      ) : (
+        <div
+          className="md:hidden inline-flex items-center px-8 py-2 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-xl md:mt-0 md:ml-4 bg-gray-900 cursor-pointer space-x-2"
+          onClick={() => setNavbarOpen(!navbarOpen)}
+        >
+          <span className="justify-center">
+            {walletconnect.provider?.accounts[0]
+              ? shortenAddress(
+                  walletconnect.provider?.accounts[0]
+                    ? walletconnect.provider?.accounts[0]
+                    : ""
+                )
+              : ""}
+          </span>
+          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+        </div>
+      )
+    ) : (
+      <XMarkIcon
+        className="h-20 w-20 text-black cursor-pointer leading-none px-3 py-1 md:hidden outline-none focus:outline-none"
+        onClick={() => setNavbarOpen(!navbarOpen)}
+      />
+    );
+
   return (
-    <div className="fixed top-0 w-full z-30 clearNav md:bg-opacity-90 transition duration-300 ease-in-out">
+    <div
+      className="fixed top-0 w-full z-30 clearNav md:bg-opacity-90 transition duration-300 ease-in-out"
+      onClick={() => {
+        if (navbarOpen) setNavbarOpen(!navbarOpen);
+      }}
+    >
       <div className="flex flex-col max-w-6xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div className="flex flex-row items-center justify-between p-4">
           <Link href="/">
@@ -18,38 +59,26 @@ const Header: FC = () => {
               </h1>
             </div>
           </Link>
-          <button
-            className="text-white cursor-pointer leading-none px-3 py-1 md:hidden outline-none focus:outline-none "
-            type="button"
-            aria-label="button"
-            onClick={() => setNavbarOpen(!navbarOpen)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="#191919"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-menu"
-            >
-              <line x1="3" y1="12" x2="21" y2="12"></line>
-              <line x1="3" y1="6" x2="21" y2="6"></line>
-              <line x1="3" y1="18" x2="21" y2="18"></line>
-            </svg>
-          </button>
+          {loginFlow()}
         </div>
+
         <div
           className={
             "md:flex flex-grow items-center" +
-            (navbarOpen ? " flex" : " hidden")
+            (navbarOpen ? " flex h-screen md:h-fit" : " hidden")
           }
         >
-          <nav className="flex-col flex-grow ">
-            <ul className="flex flex-grow justify-end flex-wrap items-center">
+          <nav
+            className={(navbarOpen ? "flex-row" : "flex-col") + " flex-grow"}
+          >
+            <ul
+              className={
+                "flex flex-wrap items-center " +
+                (navbarOpen
+                  ? "flex-col justify-center text-5xl space-y-4 md:flex-row md:space-y-0 md:text-base md:justify-end"
+                  : "md:flex-row md:space-y-0 md:text-base md:justify-end")
+              }
+            >
               <li>
                 <Link href="/#services">
                   <div className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out cursor-pointer">
@@ -64,18 +93,51 @@ const Header: FC = () => {
                   </div>
                 </Link>
               </li>
-              <li>
-                <WalletAddressDropDown
-                  address={walletconnect.provider?.accounts[0]}
-                  onDisconnect={async () => await onClose()}
-                />
-              </li>
-              <li>
-                <WalletConnectButton
-                  isLoggedIn={isLoggedIn}
-                  onOpen={async () => await onOpen()}
-                />
-              </li>
+              {!isLoggedIn ? (
+                <li>
+                  <div
+                    className="font-medium text-white hover:text-gray-600 px-5 py-3 flex items-center transition duration-150 ease-in-out cursor-pointer bg-gray-900 rounded-lg"
+                    onClick={async () => {
+                      await onOpen();
+                      setNavbarOpen(false);
+                    }}
+                  >
+                    Connect
+                  </div>
+                </li>
+              ) : (
+                <>
+                  <li>
+                    <Link href="/dashboard">
+                      <div className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out cursor-pointer">
+                        Dashboard
+                      </div>
+                    </Link>
+                  </li>
+                  <li>
+                    <div
+                      className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out cursor-pointer"
+                      onClick={async () => {
+                        await onClose();
+                        setNavbarOpen(false);
+                      }}
+                    >
+                      Disconnect
+                    </div>
+                  </li>
+                  <li>
+                    <div className="font-medium text-white hover:text-gray-600 px-2 py-2 flex items-center transition duration-150 ease-in-out cursor-pointer bg-gray-900 rounded-lg">
+                      {walletconnect.provider?.accounts[0]
+                        ? shortenAddress(
+                            walletconnect.provider?.accounts[0]
+                              ? walletconnect.provider?.accounts[0]
+                              : ""
+                          )
+                        : ""}
+                    </div>
+                  </li>
+                </>
+              )}
             </ul>
           </nav>
         </div>

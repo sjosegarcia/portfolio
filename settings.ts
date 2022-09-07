892109a5c7c4ce8560813c48f830c897297155a9
Steven Garcia <sjosegarcia23@gmail.com>
2022-09-07 16:36:16 -0400

    shortenAddress update

--- settings.ts
+++ settings.ts
@@ -1,6 +1,10 @@
 const web3FormPublicAccessKey = "2be134fe-0a11-4020-908a-eba5ec79e0ba";
-export const shortenAddress = (address: string): string =>
-  address.slice(0, 5) +
-  "..." +
-  address.slice(address.length - 4, address.length);
+export const shortenAddress = (address: string): string => {
+  if (address.length < 1) return "";
+  return (
+    address.slice(0, 5) +
+    "..." +
+    address.slice(address.length - 4, address.length)
+  );
+};
 export default web3FormPublicAccessKey;

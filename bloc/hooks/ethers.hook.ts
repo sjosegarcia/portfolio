6019a50f05855c20823f9cbb9ee846b7300223d8
Steven Garcia <sjosegarcia23@gmail.com>
2022-10-04 00:17:08 -0400

    better testing for ethers

--- bloc/hooks/ethers.hook.ts
+++ bloc/hooks/ethers.hook.ts
@@ -7,7 +7,10 @@ import {
   ethersSubject,
   initialState,
 } from "../store/ethers.store";
-import { walletConnectSubject } from "../store/walletconnect.store";
+import {
+  WalletConnectState,
+  walletConnectSubject,
+} from "../store/walletconnect.store";
 
 type EthersHook = {
   state: EthersState;
@@ -16,19 +19,20 @@ type EthersHook = {
 export const useEthers = (): EthersHook => {
   const [state, setState] = useImmer<EthersState>(initialState);
 
+  const setWeb3ProviderState = (state: WalletConnectState) => {
+    const newProvider = state.walletConnectProvider
+      ? new Web3Provider(state.walletConnectProvider)
+      : undefined;
+    ethersSubject.next({
+      web3Provider: newProvider,
+    });
+  };
+
   useEffect(() => {
     const subscriptions: Subscription[] = [
-      walletConnectSubject.subscribe((state) => {
-        const newProvider = state.walletConnectProvider
-          ? new Web3Provider(state.walletConnectProvider)
-          : undefined;
-        ethersSubject.next({
-          web3Provider: newProvider,
-        });
-      }),
+      walletConnectSubject.subscribe((state) => setWeb3ProviderState(state)),
       ethersSubject.subscribe((state) =>
         setState((draft) => {
-          console.log(state.web3Provider);
           draft.web3Provider = state.web3Provider;
         })
       ),

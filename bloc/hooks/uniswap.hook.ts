df844562569c6bb6320666867fcabeab68d4ce4d
Steven Garcia <sjosegarcia23@gmail.com>
2022-10-04 23:41:31 -0400

    uniswap hook

--- bloc/hooks/uniswap.hook.ts
+++ bloc/hooks/uniswap.hook.ts
@@ -1,20 +1,21 @@
-import {
-  Pool,
-  Position,
-  NonfungiblePositionManager,
-  nearestUsableTick,
-} from "@uniswap/v3-sdk/";
-import { useState, useEffect } from "react";
-import { ethers } from "hardhat";
-import { Percent, Token, CurrencyAmount } from "@uniswap/sdk-core";
+import { Pool } from "@uniswap/v3-sdk/";
+import { Token } from "@uniswap/sdk-core";
 import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
 import { BigNumber } from "ethers/lib/ethers";
-import { useWalletConnect } from "./walletconnect.hook";
-import { Web3Provider } from "@ethersproject/providers";
+import { useEthers } from "./ethers.hook";
+import { ethers } from "ethers";
+import { abi as QuoterABI } from "@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json";
+import { useState } from "react";
 
-// default uses “http://localhost:8545”
-// can also input your own connection with "https://mainnet.infura.io/v3/<YOUR-ENDPOINT-HERE>" as an input
-type Web3ProviderState = Web3Provider | undefined;
+type UniswapHook = [() => Promise<void>] | undefined;
+type PoolAddress = string;
+type PoolState =
+  | {
+      immutables: Immutables;
+      pool?: Pool;
+    }
+  | undefined;
+
 type Immutables = {
   factory: string;
   token0: string;
@@ -34,17 +35,13 @@ type State = {
   feeProtocol: number;
   unlocked: boolean;
 };
-export const useUniswap = () => {
-  const walletConnect = useWalletConnect();
-  const walletConnectState = walletConnect.state;
 
-  const [web3Provider, setWeb3ProviderState] = useState<Web3ProviderState>();
-
-  useEffect(() => {
-    setWeb3ProviderState((state) => ({ ...state }));
-  }, []);
-  // pool address for DAI/USDC 0.05%
-  const poolAddress = "0x6c6bc977e13df9b0de53b251522280bb72383700";
+export const useUniswap = (poolAddress: PoolAddress): UniswapHook => {
+  const ethersHook = useEthers();
+  const ethersHookState = ethersHook.state;
+  const web3Provider = ethersHookState.web3Provider;
+  const [state, setState] = useState<PoolState>();
+  if (web3Provider === undefined) return;
 
   const poolContract = new ethers.Contract(
     poolAddress,
@@ -52,8 +49,14 @@ export const useUniswap = () => {
     web3Provider
   );
 
-  async function getPoolImmutables() {
-    const immutables: Immutables = {
+  const quoterContract = new ethers.Contract(
+    "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6",
+    QuoterABI,
+    web3Provider
+  );
+
+  const poolImmutables = async (): Promise<Immutables> => {
+    return {
       factory: await poolContract.factory(),
       token0: await poolContract.token0(),
       token1: await poolContract.token1(),
@@ -61,12 +64,11 @@ export const useUniswap = () => {
       tickSpacing: await poolContract.tickSpacing(),
       maxLiquidityPerTick: await poolContract.maxLiquidityPerTick(),
     };
-    return immutables;
-  }
+  };
 
-  async function getPoolState() {
+  const poolState = async (): Promise<State> => {
     const slot = await poolContract.slot0();
-    const PoolState: State = {
+    return {
       liquidity: await poolContract.liquidity(),
       sqrtPriceX96: slot[0],
       tick: slot[1],
@@ -76,15 +78,117 @@ export const useUniswap = () => {
       feeProtocol: slot[5],
       unlocked: slot[6],
     };
-    return PoolState;
-  }
+  };
 
-  async function liquidityExamples(sender: string, exampleType: number) {
+  const priceIn = (amount: number) =>
+    state?.pool?.token0
+      ? ethers.utils.parseUnits(amount.toString(), state?.pool?.token0.decimals)
+      : undefined;
+
+  const priceOut = (quote: any) =>
+    state?.pool?.token1
+      ? ethers.utils.formatUnits(quote, state?.pool?.token1.decimals)
+      : undefined;
+
+  const priceInReverse = (amount: number) =>
+    state?.pool?.token1
+      ? ethers.utils.parseUnits(amount.toString(), state?.pool?.token1.decimals)
+      : undefined;
+
+  const priceOutReverse = (quote: any) =>
+    state?.pool?.token0
+      ? ethers.utils.formatUnits(quote, state?.pool?.token0.decimals)
+      : undefined;
+
+  const quoteIn = async (amount?: BigNumber) =>
+    state?.immutables && amount
+      ? await quoterContract.callStatic.quoteExactInputSingle(
+          state.immutables.token0,
+          state.immutables.token1,
+          state.immutables.fee,
+          amount,
+          0
+        )
+      : undefined;
+
+  const quoteOut = async (amount?: BigNumber) =>
+    state?.immutables && amount
+      ? await quoterContract.callStatic.quoteExactOutputSingle(
+          state.immutables.token0,
+          state.immutables.token1,
+          state.immutables.fee,
+          amount,
+          0
+        )
+      : undefined;
+
+  const quotePrice = async (amount: number) => {
+    const input = priceIn(amount);
+    const quoted = await quoteIn(input);
+    return priceOut(quoted);
+  };
+
+  const quotePriceReverse = async (amount: number) => {
+    const input = priceInReverse(amount);
+    const quoted = await quoteOut(input);
+    return priceOutReverse(quoted);
+  };
+
+  const swapCalculation = async (amount: number, reverse = false) => {
+    if (!reverse) return await quotePrice(amount);
+    return await quotePriceReverse(amount);
+  };
+
+  const swapPrice = async (amount: number, reverse?: boolean) =>
+    state?.immutables ? await swapCalculation(amount, reverse) : undefined;
+
+  const createPool = async (
+    immutables: Immutables,
+    state: State
+  ): Promise<Pool | undefined> => {
+    const tokenA = new Token(
+      web3Provider?.network.chainId,
+      immutables.token0,
+      6,
+      "USDC",
+      "USD Coin"
+    );
+    const tokenB = new Token(
+      web3Provider?.network.chainId,
+      immutables.token1,
+      18,
+      "WETH",
+      "Wrapped Ether"
+    );
+    return new Pool(
+      tokenA,
+      tokenB,
+      immutables.fee,
+      state.sqrtPriceX96.toString(),
+      state.liquidity.toString(),
+      state.tick
+    );
+  };
+
+  const performTest = async () => {
+    const immutables = await poolImmutables();
+    const state = await poolState();
+    const pool = await createPool(immutables, state);
+    setState({ immutables: immutables, pool: pool });
+    console.log(await swapPrice(2));
+  };
+
+  return [performTest];
+};
+
+/*
+
+  const liquidityExamples = async (sender: string, exampleType: number) => {
     const immutables = await getPoolImmutables();
     const state = await getPoolState();
     const DAI = new Token(1, immutables.token0, 18, "DAI", "Stablecoin");
     const USDC = new Token(1, immutables.token1, 18, "USDC", "USD Coin");
-    const block = await web3Provider.getBlock(provider.getBlockNumber());
+    const block = await web3Provider.getBlock(web3Provider.getBlockNumber());
     const deadline = block.timestamp + 200;
 
     // create a pool
@@ -116,7 +220,7 @@ export const useUniswap = () => {
       const { calldata, value } = NonfungiblePositionManager.addCallParameters(
         position,
         {
-          slippageTolerance: new Percent(50, 10_000),
+          slippageTolerance: new Percent(50, 10000),
           recipient: sender,
           deadline: deadline,
         }
@@ -128,7 +232,7 @@ export const useUniswap = () => {
       const { calldata, value } = NonfungiblePositionManager.addCallParameters(
         position,
         {
-          slippageTolerance: new Percent(50, 10_000),
+          slippageTolerance: new Percent(50, 10000),
           deadline: deadline,
           tokenId: 1,
         }
@@ -141,7 +245,7 @@ export const useUniswap = () => {
         NonfungiblePositionManager.removeCallParameters(position, {
           tokenId: 1,
           liquidityPercentage: new Percent(1),
-          slippageTolerance: new Percent(50, 10_000),
+          slippageTolerance: new Percent(50, 10000),
           deadline: deadline,
           collectOptions: {
             expectedCurrencyOwed0: CurrencyAmount.fromRawAmount(DAI, 0),
@@ -150,5 +254,5 @@ export const useUniswap = () => {
           },
         });
     }
-  }
-};
+  }; 
+  */
